888fffae7acb6d71d6a5024e036d41ccc6b6b556
Abhilash Gupta <s8ahgupt@stud.uni-saarland.de>
2020-08-25 17:53:40 +0200

    Task 1.3

--- simple_ecommerce/user_mgmt/models.py
+++ simple_ecommerce/user_mgmt/models.py
@@ -1,3 +1,18 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.EmailField() #default max_length = 254
+    password = models.CharField(max_length=256) # Charfield for now, update if required (also length)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    datetime joined = models.DateTimeField('Time and Date joined') #Needs autoupdate when a user joins
+    enabled = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=256) # Charfield for now, update if required (also length). Also needs autoupdate.
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
