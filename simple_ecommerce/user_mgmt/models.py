afbc566697bfeb2b9ef3b394d8534433900fa7bb
Abhilash Gupta <s8ahgupt@stud.uni-saarland.de>
2020-08-26 19:58:07 +0200

    Updated task 1.3. Now extends Django's User model

--- simple_ecommerce/user_mgmt/models.py
+++ simple_ecommerce/user_mgmt/models.py
@@ -2,17 +2,13 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class User(models.Model):
-    username = models.EmailField() #default max_length = 254
-    password = models.CharField(max_length=256) # Charfield for now, update if required (also length)
-    first_name = models.CharField(max_length=40)
-    last_name = models.CharField(max_length=40)
-    datetime joined = models.DateTimeField('Time and Date joined') #Needs autoupdate when a user joins
+# No changes made to the existing User model (with unique and compulsory username
+# but non required email). There I will just enforce username to be email. That 
+# is the plan for now. Hopefully it will work.
+class UserActivationInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     enabled = models.BooleanField(default=False)
-    activation_token = models.CharField(max_length=256) # Charfield for now, update if required (also length). Also needs autoupdate.
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+    activation_token = models.CharField(max_length=100)
